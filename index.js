64868a495198834a01bfcc331e7abd11f28f7183
phanmduong <phanminhduong@hotmail.com>
2018-06-01 21:09:18 +0700

    update code

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ import { AppRegistry } from 'react-native';
 import App from './App';
 console.log("áddsa");
 jfhdsakjfhdsajkhfda fdjksahfdjksahfjdksahfjkdsahfjkdash
+fkjdsaklfjdsakl fdsa f111111111113821783927189372189372198732891738921738921738921
 AppRegistry.registerComponent('Buoi19', () => App);
