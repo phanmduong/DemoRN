2c786ea46b33f18a974cb56dac9aaacc185d32d5
Hong Ngoc <luuhongngoc.cmu@gmail.com>
2018-06-01 21:20:58 +0700

    sua

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ import React, { Component } from "react";
 import { Platform, StyleSheet, Text, View, Button } from "react-native";
 import axios from "axios";
 import Child from './Child';
+"Luu hong ngoc"
 
 const instructions = Platform.select({
   ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
